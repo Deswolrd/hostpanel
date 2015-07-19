6fdca0ceacf9ae7e9bc9b918e9c904b43ddde69a
root <root@s9ffd151c.fastvps-server.com>
2015-07-19 14:47:55 -0400

    demonstation

---
+++ SetUp/dump.sql
@@ -0,0 +1,35 @@
+-- MySQL dump 10.13  Distrib 5.5.43, for debian-linux-gnu (i686)
+--
+-- Host: localhost    Database: symfony
+-- ------------------------------------------------------
+-- Server version	5.5.43-0+deb7u1
+
+/*!40101 SET @OLD_CHARACTER_SET_CLIENT=@@CHARACTER_SET_CLIENT */;
+/*!40101 SET @OLD_CHARACTER_SET_RESULTS=@@CHARACTER_SET_RESULTS */;
+/*!40101 SET @OLD_COLLATION_CONNECTION=@@COLLATION_CONNECTION */;
+/*!40101 SET NAMES utf8 */;
+/*!40103 SET @OLD_TIME_ZONE=@@TIME_ZONE */;
+/*!40103 SET TIME_ZONE='+00:00' */;
+/*!40014 SET @OLD_UNIQUE_CHECKS=@@UNIQUE_CHECKS, UNIQUE_CHECKS=0 */;
+/*!40014 SET @OLD_FOREIGN_KEY_CHECKS=@@FOREIGN_KEY_CHECKS, FOREIGN_KEY_CHECKS=0 */;
+/*!40101 SET @OLD_SQL_MODE=@@SQL_MODE, SQL_MODE='NO_AUTO_VALUE_ON_ZERO' */;
+/*!40111 SET @OLD_SQL_NOTES=@@SQL_NOTES, SQL_NOTES=0 */;
+
+--
+-- Table structure for table `site`
+--
+
+DROP TABLE IF EXISTS `site`;
+/*!40101 SET @saved_cs_client     = @@character_set_client */;
+/*!40101 SET character_set_client = utf8 */;
+CREATE TABLE `site` (
+  `id` int(11) NOT NULL AUTO_INCREMENT,
+  `name` varchar(100) COLLATE utf8_unicode_ci NOT NULL,
+  `host` varchar(255) COLLATE utf8_unicode_ci NOT NULL,
+  `port` int(11) NOT NULL,
+  `description` varchar(255) COLLATE utf8_unicode_ci NOT NULL,
+  `user` varchar(255) COLLATE utf8_unicode_ci NOT NULL,
+  `settingspath` varchar(255) COLLATE utf8_unicode_ci NOT NULL,
+  PRIMARY KEY (`id`)
+) ENGINE=InnoDB AUTO_INCREMENT=36 DEFAULT CHARSET=utf8 COLLATE=utf8_unicode_ci;
+/*!40101 SET character_set_client = @saved_cs_client */;
---
+++ SetUp/restart_nginx.sh
@@ -0,0 +1,26 @@
+#!/bin/bash
+cd /var/nginx/
+if [ -f /var/nginx/sites/need_restart ]; then
+rm /var/nginx/sites/need_restart
+/etc/init.d/nginx stop
+/etc/init.d/nginx start
+fi
+
+if [ -f /var/nginx/sites/need_user_manage ]; then
+LINES=()
+while IFS='' read -r line || [[ -n $line ]]; do
+	LINES+=("$line")
+done < "/var/nginx/sites/need_user_manage"
+rm /var/nginx/sites/need_user_manage
+/etc/init.d/php5-fpm stop
+for i in ${!LINES[@]}; do
+	IFS=' ' read -a array <<< "${LINES[i]}"
+	if [ "${array[0]}" == "add" ]; then
+		useradd -p $(openssl passwd -1 "${array[2]}") "${array[1]}"
+	fi
+	if [ "${array[0]}" == "del" ]; then
+		userdel "${array[1]}"
+	fi
+done
+/etc/init.d/php5-fpm start
+fi
